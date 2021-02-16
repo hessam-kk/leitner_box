6b493b2670b936bc4cae87f39223160640c33dea
Hessam <hessam2koucheki@gmail.com>
2021-02-16 23:17:43 +0330

    add feature: theme-switching

--- src/app.cpp
+++ src/app.cpp
@@ -1,6 +1,7 @@
 #include "../include/app.hpp"
 
 #include <iostream>
+#include <deque>
 
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
@@ -14,21 +15,23 @@ using namespace std;
 
 void app()
 {
+    string Theme_mode[2] = {"light", "dark"};
+    string Theme_color[4] = {"blue", "red", "green", "yellow"};
+    int curr_mod = 0, curr_color = 0; 
     sf::RenderWindow window(sf::VideoMode(window_x, window_y), "Leitner Box", sf::Style::Close); //size:  (9  in  16)  * 60
     window.requestFocus();
     // Add a Background
     sf::Texture background_texture;
-    if (!background_texture.loadFromFile("../assets/images/background-blue-light.png"))
+    if (!background_texture.loadFromFile("../assets/images/background-" + Theme_color[curr_color] + Theme_mode[curr_mod] + ".png"))
         cout << "Error On Loading BackGround Image" << endl;
     sf::Sprite background_sprite;
     background_sprite.setTexture(background_texture);
     // End Of Adding Background
 
-
-     // Main Loop
+    // Main Loop
     while (window.isOpen())
     {
-         sf::Event event;
+        sf::Event event;
         while (window.pollEvent(event))
         {
             // Close Window
