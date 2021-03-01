b314f1c7b6c2a0e7df054bdd2cb25668979f3a7b
Hessam <hessam2koucheki@gmail.com>
2021-03-01 12:19:51 +0330

    add struct last_test and ++ for User Class

--- include/user.hpp
+++ include/user.hpp
@@ -1,16 +1,27 @@
 #pragma once
 #include <iostream>
 #include <string>
-
+struct last_test
+{
+    unsigned short int total_questions;
+    double avg_score;
+    unsigned int corrects;
+    unsigned int wrongs;
+};
 class User
 {
 public:
+    User();
+    User(std::string);
     void set_password(std::string const &);
     void set_username(std::string const &);
     std::string get_password() const;
     std::string get_username() const;
-
+    User operator++();
 private:
-    std::string username;
-    std::string password;
+    std::string username = "";
+    std::string password = "";
+    unsigned int total_tests = 0;
+    double avg_scores = 0;
+    last_test last_one;
 };
--- src/user.cpp
+++ src/user.cpp
@@ -2,11 +2,18 @@
 #include <iostream>
 #include <string>
 
-void User::set_password(std::string const & x)
+User::User() {}
+
+User::User(std::string x)
+{
+    this->username = "404";
+}
+
+void User::set_password(std::string const &x)
 {
     this->password = x;
 }
-void User::set_username(std::string const & x)
+void User::set_username(std::string const &x)
 {
     this->username = x;
 }
@@ -17,4 +24,12 @@ std::string User::get_password() const
 std::string User::get_username() const
 {
     return this->username;
+}
+
+User User::operator++()
+{
+    // Calcualte Last Test statistics
+    last_one.avg_score = last_one.corrects - (1/3 * last_one.wrongs) / last_one.total_questions;
+    // Calculate the new avg score and increase total tests count
+    avg_scores = (total_tests * avg_scores + last_one.avg_score) / ++total_tests;
 }
