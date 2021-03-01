5b684abdeb061c7edae68d5a50008311bc617876
Hessam <hessam2koucheki@gmail.com>
2021-03-01 19:55:37 +0330

    add Learn and Exam button to choose

--- src/choose.cpp
+++ src/choose.cpp
@@ -22,6 +22,27 @@ bool choose()
     background_sprite.setTexture(background_texture);
     // End add a Background
 
+    // Add Learn Icon
+    sf::Texture learn_texture, learn_texture_hover;
+    if (!learn_texture.loadFromFile("../assets/images/NewRed/Learn_1.png"))
+        cout << "Error On Loading learn Image" << endl;
+    if (!learn_texture_hover.loadFromFile("../assets/images/NewRed/Learn-hover.png"))
+        cout << "Error On Loading learn-hover Image" << endl;
+    sf::Sprite learn_sprite;
+    learn_sprite.setTexture(learn_texture);
+    learn_sprite.setPosition(400, 400);
+    // End add Learn Icon
+    // Add Exam Icon
+    sf::Texture Exam_texture, Exam_texture_hover;
+    if (!Exam_texture.loadFromFile("../assets/images/NewRed/Exam_1.png"))
+        cout << "Error On Loading Exam Image" << endl;
+    if (!Exam_texture_hover.loadFromFile("../assets/images/NewRed/Exam-hover.png"))
+        cout << "Error On Loading Exam-hover Image" << endl;
+    sf::Sprite Exam_sprite;
+    Exam_sprite.setTexture(Exam_texture);
+    Exam_sprite.setPosition(870, 400);
+    // End add Learn Icon
+
     while (window.isOpen())
     {
         sf::Event event;
@@ -33,9 +54,48 @@ bool choose()
             {
                 window.close();
             }
+             // Mouse Click
+            if (event.type == sf::Event::MouseButtonPressed)
+            {
+                // Click on Learn
+                if (learn_sprite.getGlobalBounds().contains(sf::Vector2f(event.mouseButton.x, event.mouseButton.y)))
+                {
+                    cout << " Learn" << endl;
+                    return 0;
+                }
+                // Click on Exam
+                if (Exam_sprite.getGlobalBounds().contains(sf::Vector2f(event.mouseButton.x, event.mouseButton.y)))
+                {
+                    cout << " Exam" << endl;
+                    return 1;
+                }
+            }
+             // Mouse Move
+            if (event.type == sf::Event::MouseMoved)
+            {
+                // Exam Icon
+                if (Exam_sprite.getGlobalBounds().contains(sf::Vector2f(event.mouseMove.x, event.mouseMove.y)))
+                {
+                    Exam_sprite.setTexture(Exam_texture_hover);
+                }
+                else
+                {
+                    Exam_sprite.setTexture(Exam_texture);
+                }
+                // learn Icon
+                if (learn_sprite.getGlobalBounds().contains(sf::Vector2f(event.mouseMove.x, event.mouseMove.y)))
+                {
+                    learn_sprite.setTexture(learn_texture_hover);
+                }
+                else
+                {
+                    learn_sprite.setTexture(learn_texture);
+                }
+            }
         }
-    window.draw(background_sprite);
-    window.display();
+        window.draw(background_sprite);
+        window.draw(learn_sprite);
+        window.draw(Exam_sprite);
+        window.display();
     }
-
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
 #include "../include/app.hpp"
 #include "../include/login.hpp"
+#include "../include/choose.hpp"
 using namespace std;
-
+struct last_test
+{
+    unsigned short int total_questions = 0;
+    double avg_score = 0;
+    unsigned int corrects = 0;
+    unsigned int wrongs = 0;
+};
 int main()
 {
-    if (login())
+    // login();
+    if (choose() == 0) // learn
     {
-        app();
+        // learn();
     }
+    else
+    {
+        // exam();
+    }
+    // app();
     return 0;
 }
