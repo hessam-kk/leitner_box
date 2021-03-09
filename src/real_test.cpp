aedf299e68c01049f67115608cf05605bdb8f5b8
Hessam <hessam2koucheki@gmail.com>
2021-03-09 19:28:11 +0330

    Create a word_list for test

--- src/real_test.cpp
+++ src/real_test.cpp
@@ -1,5 +1,10 @@
 #include "real_test.hpp"
-#include "../include/word.hpp"
+#include "word.hpp"
+#include "word_in_test.hpp"
+#include "file.hpp"
+
+#include <stdlib.h>
+#include <time.h>
 #include <iostream>
 #include <string>
 #include <deque>
@@ -8,6 +13,7 @@
 #include <SFML/Window.hpp>
 #include <SFML/System.hpp>
 
+using std::array;
 using std::cout;
 using std::deque;
 using std::endl;
@@ -23,6 +29,67 @@ void real_test(User &Primary)
     sf::Sprite background_sprite;
     background_sprite.setTexture(background_texture);
     // End Of Adding Background
+
+    deque<string> tested_word;
+    deque<string> meaning;
+    deque<string> other_meaning;
+
+    int cur_list;
+    sf::Text cur_word, correct_mean, wrong_mean;
+    array<deque<Word>, 7> arr;
+    read_file(arr, Primary.get_username());
+    deque<Word> total_words;
+    // Store all words in a single list to use shuffle mode
+    for (size_t i = 0; i < 7; i++)
+    {
+        for (auto const &it : arr[i])
+        {
+            total_words.push_back(it);
+        }
+    }
+    deque<Word> test_word_list;
+    if (total_words.size() > 10)
+    {
+        while (test_word_list.size() < 10)
+        {
+            srand(time(NULL));
+            Word_in_test tmp;
+            // Setting Word
+            int place = rand() % (total_words.size() - 1);
+            std::string word = total_words.at(place).get_word();
+            tmp.set_word(word);
+            // Setting
+            std::string mean = total_words.at(place).get_word();
+            tmp.set_meaning(mean);
+            // Removing word to avoid duplication
+            total_words.erase(total_words.begin() + place);
+            srand(time(NULL));
+            int place = rand() % (total_words.size() - 1);
+            tmp.set_wrong_meaning(total_words[place].get_word());
+            test_word_list.push_back(tmp);
+        }
+    }
+    else
+    {
+        for (size_t i = 0; i < total_words.size() / 2; i++)
+        {
+            srand(time(NULL));
+            Word_in_test tmp;
+            // Setting Word
+            int place = rand() % (total_words.size() - 1);
+            std::string word = total_words.at(place).get_word();
+            tmp.set_word(word);
+            // Setting
+            std::string mean = total_words.at(place).get_word();
+            tmp.set_meaning(mean);
+            // Removing word to avoid duplication
+            total_words.erase(total_words.begin() + place);
+            srand(time(NULL));
+            int place = rand() % (total_words.size() - 1);
+            tmp.set_wrong_meaning(total_words[place].get_word());
+            test_word_list.push_back(tmp);
+        }
+    }
 
     while (window.isOpen())
     {
